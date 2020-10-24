7cab953d7efedec3909f26f0e283363df4ba030a
JustLiving <JustinTLivingston@gmail.com>
2020-10-23 17:23:50 -0700

    Add junit test

--- JPANBATeam/src/main/java/com/skilldistillery/jpacrud/entities/NBATeam.java
+++ JPANBATeam/src/main/java/com/skilldistillery/jpacrud/entities/NBATeam.java
@@ -14,20 +14,130 @@ public class NBATeam {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	private String name;
-	
+
 	private String city;
-	
+
 	private int championships;
-	
+
 	private String coach;
-	
+
 	private String conference;
-	
+
+	@Column(name = "year_inserted")
 	private LocalDateTime yearFounded;
-	
+
 	@Column(name = "team_goat")
 	private String teamGoat;
+
+	public NBATeam(String name, String city, int championships, String coach, String conference,
+			LocalDateTime yearFounded, String teamGoat) {
+		super();
+		this.name = name;
+		this.city = city;
+		this.championships = championships;
+		this.coach = coach;
+		this.conference = conference;
+		this.yearFounded = yearFounded;
+		this.teamGoat = teamGoat;
+	}
+
+	public NBATeam() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getChampionships() {
+		return championships;
+	}
+
+	public void setChampionships(int championships) {
+		this.championships = championships;
+	}
+
+	public String getCoach() {
+		return coach;
+	}
+
+	public void setCoach(String coach) {
+		this.coach = coach;
+	}
+
+	public String getConference() {
+		return conference;
+	}
+
+	public void setConference(String conference) {
+		this.conference = conference;
+	}
+
+	public LocalDateTime getYearFounded() {
+		return yearFounded;
+	}
+
+	public void setYearFounded(LocalDateTime yearFounded) {
+		this.yearFounded = yearFounded;
+	}
+
+	public String getTeamGoat() {
+		return teamGoat;
+	}
+
+	public void setTeamGoat(String teamGoat) {
+		this.teamGoat = teamGoat;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("NBATeam [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", city=");
+		builder.append(city);
+		builder.append(", championships=");
+		builder.append(championships);
+		builder.append(", coach=");
+		builder.append(coach);
+		builder.append(", conference=");
+		builder.append(conference);
+		builder.append(", yearFounded=");
+		builder.append(yearFounded);
+		builder.append(", teamGoat=");
+		builder.append(teamGoat);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+
+	
 	
 }
---
+++ JPANBATeam/src/test/java/com/skilldistillery/jpacrud/entities/NBATest.java
@@ -0,0 +1,57 @@
+package com.skilldistillery.jpacrud.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NBATest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private NBATeam team;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("NBAPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		team = em.find(NBATeam.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		team = null;
+	}
+
+	@Test
+	void test_nba_team() {
+		assertNotNull(team);
+		assertEquals("Laker", team.getName());
+		assertEquals("Los Angeles", team.getCity());
+		assertEquals(17, team.getChampionships());
+		assertEquals("Western", team.getConference());
+		LocalDateTime lt = team.getYearFounded();
+		assertEquals(1946, lt.getYear());
+		assertEquals("Kobe Bryant", team.getTeamGoat());
+	}
+
+}
